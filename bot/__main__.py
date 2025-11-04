e9db9d11c104c4ea09631762585d84bd3b639751
EmmaStone830 <athururohitreddy@gmail.com>
2025-11-04 07:42:40 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,3 +1,4 @@
+import asyncio
 from asyncio import create_task, create_subprocess_exec, create_subprocess_shell, run as asyrun, all_tasks, gather, sleep as asleep
 from aiofiles import open as aiopen
 from pyrogram import idle
@@ -6,19 +7,20 @@ from os import path as ospath, execl, kill
 from sys import executable
 from signal import SIGKILL
 
-from bot import bot, Var, bot_loop, sch, LOGS, ffQueue, ffLock, ffpids_cache, ff_queued
+from bot import bot, Var, sch, LOGS, ffQueue, ffLock, ffpids_cache, ff_queued
 from bot.core.auto_animes import fetch_animes
 from bot.core.func_utils import clean_up, new_task, editMessage
 from bot.modules.up_posts import upcoming_animes
 
+
 @bot.on_message(command('restart') & user(Var.ADMINS))
 @new_task
-async def restart(client, message):
+async def restart_command(client, message):
     rmessage = await message.reply('<i>Restarting...</i>')
     if sch.running:
         sch.shutdown(wait=False)
     await clean_up()
-    if len(ffpids_cache) != 0: 
+    if len(ffpids_cache) != 0:
         for pid in ffpids_cache:
             try:
                 LOGS.info(f"Process ID : {pid}")
@@ -31,6 +33,7 @@ async def restart(client, message):
         await f.write(f"{rmessage.chat.id}\n{rmessage.id}\n")
     execl(executable, executable, "-m", "bot")
 
+
 async def restart():
     if ospath.isfile(".restartmsg"):
         with open(".restartmsg") as f:
@@ -39,7 +42,8 @@ async def restart():
             await bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text="<i>Restarted !</i>")
         except Exception as e:
             LOGS.error(e)
-            
+
+
 async def queue_loop():
     LOGS.info("Queue Loop Started !!")
     while True:
@@ -52,21 +56,27 @@ async def queue_loop():
                 ffQueue.task_done()
         await asleep(10)
 
+
 async def main():
     sch.add_job(upcoming_animes, "cron", hour=0, minute=30)
     await bot.start()
     await restart()
     LOGS.info('Auto Anime Bot Started!')
     sch.start()
-    bot_loop.create_task(queue_loop())
+
+    # Create background task
+    asyncio.create_task(queue_loop())
+
     await fetch_animes()
     await idle()
     LOGS.info('Auto Anime Bot Stopped!')
+
     await bot.stop()
-    for task in all_tasks:
+    for task in asyncio.all_tasks():
         task.cancel()
     await clean_up()
     LOGS.info('Finished AutoCleanUp !!')
-    
+
+
 if __name__ == '__main__':
-    bot_loop.run_until_complete(main())
+    asyncio.run(main())
